f86062424ecf14c11eafbd81cdb10e3c6ef20b0c
ndc <ndc@localhost.localdomain>
2024-05-12 06:21:37 -0300

    Initial Commit

---
+++ frontend.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+from table import pipeline
+import fitz
+
+st.set_page_config(layout='wide')
+
+def count_single_class(uploaded_file,) -> pd.DataFrame:
+    if uploaded_file is not None:
+        st.write(uploaded_file.name)
+        bytes_data = uploaded_file.getvalue()
+        data = fitz.open('pdf', bytes_data)
+        table = pipeline(data)
+        st.table(table)
+        return table
+
+def main():
+    summation = None
+    uploaded_files = st.file_uploader('Choose a file', type='pdf', accept_multiple_files=True)
+    for file in uploaded_files:
+        current_class = count_single_class(file)
+        if summation is None:
+            summation = pd.DataFrame({'nome':current_class.nome.values})
+        summation[file.name.split('.')[0]] = current_class[['soma']]
+    if summation is not None:
+        summation['soma'] = summation.drop(columns=['nome']).sum(axis=1)
+        st.table(summation)
+        summation.to_excel('summation.xlsx')
+    return summation
+
+if __name__ == '__main__':
+    main()
---
+++ table.py
@@ -0,0 +1,59 @@
+import fitz
+import pandas as pd
+
+
+def fitz_doc_to_table(doc:bytes) -> pd.DataFrame:
+    result = list()
+    columns = None
+    for page in doc:
+        tables = page.find_tables()
+        for table in tables:
+            rows = table.extract()
+            for (index, row) in enumerate(rows):
+                if index == 3:
+                    columns = row
+                if len(row) == 60 and row[0].isdigit() and row[-1].isdigit():
+                    result.append(row)
+    columns[1] = 'nome'
+    return pd.DataFrame(result, columns=columns)
+
+def pdf_to_table(pdf_path:str) -> pd.DataFrame:
+    doc = fitz.open(pdf_path)
+    return fitz_doc_to_table(doc)
+
+def filter_df_by_month(df:pd.DataFrame, month:str) -> pd.DataFrame:
+    names = list(df.pop('nome').values)
+    dates = df.filter(regex=f'/{month}/')
+    dates['nome'] = names
+    return dates
+
+def sum_fs(df:pd.DataFrame) -> pd.DataFrame:
+    df['soma'] = df.apply(lambda x: len([f for f in x if f=='F']), axis=1)
+    return df
+
+
+def pipeline(doc:bytes, month:str='04') -> pd.DataFrame:
+    df = fitz_doc_to_table(doc)
+    df = filter_df_by_month(df, month)
+    df = sum_fs(df)
+    df = _differentiate_repeated_columns(df)
+    return df
+
+
+def _differentiate_repeated_columns(df: pd.DataFrame) -> pd.DataFrame:
+    column_counts = {}
+    new_columns = []
+    
+    for column in df.columns:
+        if column in column_counts:
+            column_counts[column] += 1
+            new_columns.append(f'{column}-{column_counts[column]}')
+        else:
+            column_counts[column] = 0
+            new_columns.append(column)
+    df.columns = new_columns
+    return df
+if __name__ == '__main__':
+    doc = fitz.open('index.pdf')
+    df = pipeline(doc)
+    print(df)
